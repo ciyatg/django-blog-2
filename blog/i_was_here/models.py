47e6b9d145879314689f9f2af70f0a59ccab7276
ciya <ciya>
2017-09-21 16:11:16 +0200

    for simplicity removed the date attribute

--- blog/i_was_here/models.py
+++ blog/i_was_here/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class was_here(models.Model): #Post inherits from models.Model
     name = models.CharField(max_length = 40)
-    date_of_visit = models.DateTimeField()
 
     def __str__(self):
         return self.name
--- blog/i_was_here/urls.py
+++ blog/i_was_here/urls.py
@@ -4,9 +4,7 @@ from django.views.generic import ListView, DetailView
 from i_was_here.models import was_here
 
 urlpatterns = [ url(r'^$', ListView.as_view(
-                                    queryset=was_here.objects.all().order_by("-date")[:20],
-                                    template_name="posts/posts.html")),
-                url(r'^(?P<pk>\d+)$', DetailView.as_view( #pk means primary key, \d means digit, + means "one or more", \d+ meeans one or more digits.
-                                    model = was_here,
-                                    template_name="posts/post.html")),
+                                    queryset=was_here.objects.all(),
+                                    template_name="i_was_here/posts.html")),
+                
                 ]
