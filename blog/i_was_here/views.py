483957723ddeada54243dc8ec9d05b051f138203
ciya <ciya>
2017-09-22 18:46:34 +0200

    Primitive version of the i_Was_here page is ready.

--- blog/i_was_here/views.py
+++ blog/i_was_here/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import WasHere #The dot before models means current directory or
+#current application. Both views.py and models.py are in the same directory.
+#This means we can use . and the name of the file (without .py). Then we import the name of the model (WasHere).
+from django.utils import timezone
 
 def here_list(request):
-    return render(request, 'i_was_here/here_list.html', {})
+    people = WasHere.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #Orders and publishs who was there. We will pass "people" to the template.
+    return render(request, 'i_was_here/here_list.html', {'people': people})
