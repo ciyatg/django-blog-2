03f73a54307e62e744dfa2f3cdfddc71ebccf0f0
ciya <ciya>
2017-09-22 22:04:29 +0200

    Created form to add a new name.

--- blog/i_was_here/forms.py
+++ blog/i_was_here/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from .models import WasHere # Because we want to create a form for the WasHere model.
 
-class form_was_here(forms.Form):
-    person = forms.CharField(max_length = 40)
+class NameForm(forms.ModelForm):
 
+    class Meta:
+        model = WasHere #Django will use WasHere model for the form.
+        fields = ('Name',)
 
 
