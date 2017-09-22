0b18c373ab528e0ee86d0a47ba0629c72d5cf055
ciya <ciya>
2017-09-22 16:12:05 +0200

    redesigned the model

--- blog/i_was_here/models.py
+++ blog/i_was_here/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 #Each class is a table, each variable is a column in the database.
 
-class was_here(models.Model): #Post inherits from models.Model
+class WasHere(models.Model): #Post inherits from models.Model. With this way Django knows that WasHere is a model and should be saved in the database.
     name = models.CharField(max_length = 40)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self): #This is the publish method.
+        self.published_date = timezone.now()
+        self.save()
+
 
     def __str__(self):
         return self.name
