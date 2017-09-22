fc0eeef9f8c067aa4b34374a6e6384ba6816826b
ciya <ciya>
2017-09-22 23:41:32 +0200

    After making changes in views.py and post_edit.html now i_was_here app is working.

--- blog/i_was_here/views.py
+++ blog/i_was_here/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import WasHere #The dot before models means current directory or
 #current application. Both views.py and models.py are in the same directory.
 #This means we can use . and the name of the file (without .py). Then we import the name of the model (WasHere).
@@ -11,5 +11,13 @@ def here_list(request):
     return render(request, 'i_was_here/here_list.html', {'people': people})
 
 def post_new(request):
-    form = NameForm()
+    if request.method == "POST": #The result of request.method == "POST" is a boolean value - True if the current request from a user was performed using the HTTP "POST" method, of False otherwise
+        form = NameForm(request.POST) # request.POST adds "this field is required" statement and does not accept an empy fied.
+        if form.is_valid(): #checks if everything okay is, for example if all the required fields are filled.
+            post = form.save(commit=False) #says "wait to save the form"
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('here_list') # "here_list" is name of a urlpattern. See urls.py.
+    else:
+        form = NameForm()
     return render(request, 'i_was_here/post_edit.html', {'form': form})
